31ffa6f0c679cf72ad2b2657a16e283e12090a58
dixitpatel9912 <you@example.com>
2019-12-12 14:35:47 -0600

    hi

--- file_handling.py
+++ file_handling.py
@@ -1,42 +1,60 @@
 import re
-f = open("nirav.txt", "r+")
-data = f.read()
-numofword = len(data.split())
-print(data)
-print(numofword)
+# f = open("nirav.txt", "r+")
+# data = f.read()
+# numofword = len(data.split())
+# print(data)
+# print(numofword)
+#
+#
+# def four_letters():
+#     f = open("nirav.txt", "r+")
+#     data = f.read()
+#     test = data.split()
+#     ee = [i for i in test if len(i) == 4]
+#     eee = ee.__len__()
+#     return eee
+#
+#
+# # print(four_letters())
+# def the():
+#     count = 0
+#     f = open('nirav.txt', 'r+')
+#     data = f.read()
+#     tu = re.findall('the', data)
+#     ee = tu.__len__()
+#     return ee
+#
+#
+# print(the())
+#
+#
+# def replace():
+#     count = 0
+#     f = open('nirav.txt', 'r+')
+#     data = f.read()
+#     kkk = re.sub('Scrooge', 'the Grinch', data)
+#     return kkk
+#
+#
+# print(replace())
+# import os
+# from os import path
+#
+#
+# def main():
+#     if path.exists("nirav.txt"):
+#         src = path.realpath("nirav.txt");
+#         os.rename("nirav.txt", "grinch-version.txt")
+#
+#
+# if __name__ == "__main__":
+#     main()
+y = 8
 
+def test():
+    z = y + 1
+    return z
 
-def four_letters():
-    f = open("nirav.txt", "r+")
-    data = f.read()
-    test = data.split()
-    ee = [i for i in test if len(i) == 4]
-    eee = ee.__len__()
-    return eee
+y
 
-
-import re
-
-
-# print(four_letters())
-def the():
-    count = 0
-    f = open('nirav.txt', 'r+')
-    data = f.read()
-    tu = re.findall('the', data)
-    ee = tu.__len__()
-    return ee
-
-
-print(the())
-
-
-def replace():
-    count = 0
-    f = open('nirav.txt', 'r+')
-    data = f.read()
-    kkk = re.sub('Scrooge', 'the Grinch', data)
-    return kkk
-
-
-print(replace())
+print(test())
